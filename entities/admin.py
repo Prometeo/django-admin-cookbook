ee48e14d73992724a1a3d3d32a9fc808fad5da73
xairi <xairi.valdivia@glr.pe>
2018-11-06 18:51:10 -0500

    Enable filtering on calculated fields

--- entities/admin.py
+++ entities/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Category, Hero, Villain, Origin
 # Register your models here.
 
@@ -7,14 +8,22 @@ from .models import Category, Hero, Villain, Origin
 class OriginAdmin(admin.ModelAdmin):
     list_display = ("name", "hero_count", "villain_count")
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _hero_count=Count("hero", distinct=True),
+            _villain_count=Count("villain", distinct=True),
+        )
+        return queryset
+
     def hero_count(self, obj):
-        return obj.hero_set.count()
+        return obj._hero_count
 
     def villain_count(self, obj):
-        return obj.villain_set.count()
+        return obj._villain_count
 
-    hero_count.admin_order_field = 'hero_count'
-    villain_count.admin_order_field = 'villain_count'
+    hero_count.admin_order_field = '_hero_count'
+    villain_count.admin_order_field = '_villain_count'
 
 
 @admin.register(Category)
@@ -24,7 +33,31 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Hero)
 class HeroAdmin(admin.ModelAdmin):
-    pass
+
+    class IsVeryBenevolentFilter(admin.SimpleListFilter):
+        title = 'is_very_benevolent'
+        parameter_name = 'is_very_benevolent'
+
+        def lookups(self, request, model_admin):
+            return (
+                ('Yes', 'Yes'),
+                ('No', 'No'),
+            )
+
+        def queryset(self, request, queryset):
+            value = self.value()
+            if value == 'Yes':
+                return queryset.filter(benevolence_factor__gt=75)
+            elif value == 'No':
+                return queryset.exclude(benevolence_factor__gt=75)
+            return queryset
+
+    list_display = ("name", "is_immortal", "category",
+                    "origin", "is_very_benevolent")
+    list_filter = ("is_immortal", "category", "origin", IsVeryBenevolentFilter)
+
+    def is_very_benevolent(self, obj):
+        return obj.benevolence_factor > 75
 
 
 @admin.register(Villain)
