a749804e3a9732b65b559a405f1179db745e66c8
xairi <xairi.valdivia@glr.pe>
2018-11-07 17:00:36 -0500

    Import csv

--- entities/admin.py
+++ entities/admin.py
@@ -1,8 +1,10 @@
 import csv
+from django import forms
 from django.db.models import Count
 from django.contrib import admin
 from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import path
 from .models import Category, Hero, Villain, Origin
 # Register your models here.
@@ -56,6 +58,10 @@ class ExportCsvMixin:
     export_as_csv.short_description = "Export Selected"
 
 
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
+
 @admin.register(Hero)
 class HeroAdmin(admin.ModelAdmin, ExportCsvMixin):
     change_list_template = "entities/heroes_changelist.html"
@@ -65,8 +71,23 @@ class HeroAdmin(admin.ModelAdmin, ExportCsvMixin):
         my_urls = [
             path('immortal/', self.set_immortal),
             path('mortal/', self.set_mortal),
+            path('import-csv/', self.import_csv),
         ]
         return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            reader = csv.reader(csv_file)
+            # Create Hero objects from passed in data
+            # ...
+            self.message_user(request, "Your csv file has been imported")
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(
+            request, "admin/csv_form.html", payload
+        )
 
     def set_immortal(self, request):
         self.model.objects.all().update(is_immortal=True)
